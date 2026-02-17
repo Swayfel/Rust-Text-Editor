8e95a18fb50d3b818220750c4428522cd39fe511
Swayfel <marcus.steuer.1@gmx.de>
2026-02-17 22:10:39 +0100

    Add new file for editor logic and change code to idiomatic russt

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,8 @@
- use std::io::{self, Read};
- use crossterm::terminal::enable_raw_mode;
- use crossterm::terminal::disable_raw_mode;
+#![warn(clippy::all, clippy::pedantic)]
+mod editor;
+use editor::Editor;
 
 fn main() {
-    enable_raw_mode().unwrap();
-    for b in io::stdin().bytes(){
-        let c = b.unwrap() as char;
-        println!("{}", c);
-
-        if c == 'q'{
-            disable_raw_mode().unwrap();
-            break;
-        }
-    }
-
+    let editor = Editor::default();
+    editor.run();
 }
